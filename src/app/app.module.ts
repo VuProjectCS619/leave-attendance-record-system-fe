29f4c5f09c86096197036d3fbc00c0646c2ad9ba
osamaa15 <osaift@gmail.com>
2023-02-06 21:26:37 +0500

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AccountModule} from "./account/account.module";
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
