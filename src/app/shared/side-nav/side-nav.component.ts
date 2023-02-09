dab0bcb4aedf2d32245766cfa47a8cb2c06ce9e6
osamaa15 <osaift@gmail.com>
2023-02-09 21:32:19 +0500

    chore: conditional navbar component implemented

--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent {
-  user: string = "employee"
-  title:string = ""
+  user: string = "admin"
+  greeting:string = "Muhammad Osama Iftikhar"
 }
